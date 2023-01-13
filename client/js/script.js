7c7905220a84a0d671da491e565ca9eda4c429ce
h21tisto <h21tisto@du.se>
2023-01-13 14:34:42 +0100

    .

--- client/js/script.js
+++ client/js/script.js
@@ -7,19 +7,33 @@ let albumNameValid = false;
 let albumArtValid = true;
 let songLengthValid = true;
 
+let playlistNameValid = false;
+let playlistFileNameValid = false;
+let playlistAuthorsValid = false;
+
 let selectedPlaylist = -1;
+let submitType = "playlist";
 
-const form = document.getElementById('form')
-const songName = form.addName;
-const year = form.addYear;
-const artists = form.addArtists;
-const albumName = form.addAlbumName;
-const albumArt = form.addAlbumArt;
-const songLength = form.addLength;
+const songForm = document.getElementById('songForm')
+const songName = songForm.addName;
+const year = songForm.addYear;
+const artists = songForm.addArtists;
+const albumName = songForm.addAlbumName;
+const albumArt = songForm.addAlbumArt;
+const songLength = songForm.addLength;
 
+const playlistForm = document.getElementById('playlistForm');
+const playlistName = playlistForm.addPlaylistName;
+const playlistFileName = playlistForm.addFileName;
+const playlistAuthors = playlistForm.addAuthors;
+
+const sidebar = document.getElementById('sidebar');
 const playlist = document.getElementById('playlist');
+const toggleButton = document.getElementById('toggleSelect');
+const contentHeader = document.getElementById('content__header');
+const sidebarHeader = document.getElementById('sidebar__header');
 
-form.addEventListener('submit', onSubmit);
+songForm.addEventListener('submit', onSubmit);
 year.addEventListener('blur', (e) => validateField(e.target));
 year.addEventListener('input', (e) => validateField(e.target));
 artists.addEventListener('blur', (e) => validateField(e.target));
@@ -100,6 +114,36 @@ function validateField(field) {
             }
             break;
         }
+        case 'addPlaylistName': {
+            if (value.length === 0){
+                playlistNameValid = false;
+                validationMessage = "Fältet 'Playlist name' är obligatoriskt!";
+            }
+            else {
+                playlistNameValid = true;
+            }
+            break;
+        }   
+        case 'addFileName': {
+            if (value.length === 0){
+                playlistFileNameValid = false;
+                validationMessage = "Fältet 'File name' är obligatoriskt!";
+            }
+            else {
+                playlistFileNameValid = true;
+            }
+            break;
+        }
+        case 'addAuthors': {
+            if (value.length === 0){
+                playlistAuthorsValid = false;
+                validationMessage = "Fältet 'Author(s)' är obligatoriskt!";
+            }
+            else {
+                playlistAuthorsValid = true;
+            }
+            break;
+        } 
     }
 
     const errorMessage = field.previousElementSibling.children[1];
@@ -110,53 +154,80 @@ function validateField(field) {
 function onSubmit(e) {
     e.preventDefault();
     let validationMessage = '';
-
-    if (!songNameValid) {
-        validationMessage = "Fältet 'Song name' är obligatoriskt!";
-        document.getElementById('songError').innerText = validationMessage;
-        document.getElementById('songError').classList.remove('hidden');
+    console.log(submitType);
+    if (submitType === "song") {
+        if (!songNameValid) {
+            validationMessage = "Fältet 'Song name' är obligatoriskt!";
+            document.getElementById('songError').innerText = validationMessage;
+            document.getElementById('songError').classList.remove('hidden');
+        }
+    
+        if (!yearValid) {
+            validationMessage = "Fältet 'Release year' är obligatoriskt!";
+            document.getElementById('yearError').innerText = validationMessage;
+            document.getElementById('yearError').classList.remove('hidden');
+        }
+    
+        if (!artistsValid) {
+            validationMessage = "Fältet 'Artists' är obligatoriskt!";
+            document.getElementById('artistsError').innerText = validationMessage;
+            document.getElementById('artistsError').classList.remove('hidden');
+        }
+    
+        if (!albumNameValid) {
+            validationMessage = "Fältet 'Album name' är obligatoriskt!";
+            document.getElementById('albumError').innerText = validationMessage;
+            document.getElementById('albumError').classList.remove('hidden');
+        }
+    
+        if (selectedPlaylist === -1) {
+            validationMessage = "Du måste välja en spellista först!";
+            document.getElementById('playListError').innerText = validationMessage;
+            document.getElementById('playListError').classList.remove('hidden');
+        }
+    
+        if (songNameValid && yearValid && artistsValid && albumNameValid && selectedPlaylist !== -1){
+            saveSong();
+        }
     }
+    else if (submitType === "playlist") {
+        console.log('test');
+        if (!playlistNameValid) {
+            validationMessage = "Fältet 'Playlist name' är obligatoriskt!";
+            document.getElementById('playlistNameError').innerText = validationMessage;
+            document.getElementById('playlistNameError').classList.remove('hidden');
+        }
+    
+        if (!playlistFileNameValid) {
+            validationMessage = "Fältet 'File name' är obligatoriskt!";
+            document.getElementById('filenameError').innerText = validationMessage;
+            document.getElementById('filenameError').classList.remove('hidden');
+        }
+    
+        if (!playlistAuthorsValid) {
+            validationMessage = "Fältet 'Authors' är obligatoriskt!";
+            document.getElementById('authorsError').innerText = validationMessage;
+            document.getElementById('authorsError').classList.remove('hidden');
+        }
 
-    if (!yearValid) {
-        validationMessage = "Fältet 'Release year' är obligatoriskt!";
-        document.getElementById('yearError').innerText = validationMessage;
-        document.getElementById('yearError').classList.remove('hidden');
+        if (songNamplaylistNameValideValid && playlistFileNameValid && playlistAuthorsValid){
+            savePlaylist();
+        }
     }
-
-    if (!artistsValid) {
-        validationMessage = "Fältet 'Artists' är obligatoriskt!";
-        document.getElementById('artistsError').innerText = validationMessage;
-        document.getElementById('artistsError').classList.remove('hidden');
-    }
-
-    if (!albumNameValid) {
-        validationMessage = "Fältet 'Album name' är obligatoriskt!";
-        document.getElementById('albumError').innerText = validationMessage;
-        document.getElementById('albumError').classList.remove('hidden');
-    }
-
-    if (selectedPlaylist === -1) {
-        validationMessage = "Du måste välja en spellista först!";
-        document.getElementById('playListError').innerText = validationMessage;
-        document.getElementById('playListError').classList.remove('hidden');
-    }
-
-    if (songNameValid && yearValid && artistsValid && albumNameValid && selectedPlaylist !== -1){
-        saveSong();
-    }
+    
 }
 
 function saveSong() {
     const song = {
-        songName: form.addName.value,
-        releaseYear: form.addYear.value,
-        artists: form.addArtists.value,
-        albumName: form.addAlbumName.value,
-        albumArt: form.addAlbumArt.value,
-        songLength: form.addLength.value
+        songName: songForm.addName.value,
+        releaseYear: songForm.addYear.value,
+        artists: songForm.addArtists.value,
+        albumName: songForm.addAlbumName.value,
+        albumArt: songForm.addAlbumArt.value,
+        songLength: songForm.addLength.value
     };
     
-    api.addSongToPlaylist(0, song).then((song) => { if (song) renderPlayList(selectedPlaylist) });
+    api.addSongToPlaylist(0, song).then((song) => { if (song) renderPlayList(selectedPlaylist); });
     
     //songName.value = '';
     //year.value = '';
@@ -171,18 +242,26 @@ function saveSong() {
     //albumNameValid = false;
 }
 
-function renderPlayList(playlist_id) {
-    sidebar = document.getElementById('sidebar');
-    toggleButton = document.getElementById('toggleSelect');
-    selection = document.getElementById('content__selection');
-    contentHeader = document.getElementById('content__header');
-    sidebarHeader = document.getElementById('sidebar__header');
+function savePlaylist() {
+    const playlistData = {
+        playListName: playlistForm.addPlaylistName.value,
+        authors: playlistForm.addAuthors.value
+    };
 
+    console.log(playlistData);
+    api.createPlayList(playlistData).then((playlist) => { if (playlist) renderSelection(); });
+}
+
+function renderPlayList(playlist_id) {
+    submitType = "song";
+    const selection = document.getElementById('content__selection');
+    
     selection.remove();
     toggleButton.classList.remove('hidden');
     html = `<button onclick="renderSelection()">Välj ny spellista</button>`;
     
-    form.classList.remove('hidden');
+    songForm.classList.remove('hidden');
+    playlistForm.classList.add('hidden');
     sidebarHeader.innerText = "Lägg till låt";
     contentHeader.insertAdjacentHTML('beforeend', html);
     
@@ -191,7 +270,7 @@ function renderPlayList(playlist_id) {
         playlist.innerHTML = '';
         console.log(result.id);
         selectedPlaylist = result.id;
-        header.innerText = result.playListName;
+        contentHeader.innerText = result.playListName;
         // README: Resultatet vi får tbx är spellistan i format av vår JSON-Schema.
         // Plocka ut informationen ni behöver ifrån den!
         if (result) {
@@ -201,82 +280,26 @@ function renderPlayList(playlist_id) {
 }
 
 function renderSelection() {
-    content = document.getElementById('content');
-    header = document.getElementById('content__header');
-    sidebarHeader = document.getElementById('sidebar__header');
-    toggleButton = document.getElementById('toggleSelect');
-    selection = document.getElementById('content__selection');
-    
     selectedPlaylist = -1;
+    submitType = "playlist";
     playlist.innerHTML = '';
-    header.innerText = "Välj Spellista";
+    contentHeader.innerText = "Välj Spellista";
     sidebarHeader.innerText = "Lägg till spellista";
 
-    form.classList.add('hidden');
+    songForm.classList.add('hidden');
     toggleButton.classList.add('hidden');
+    playlistForm.classList.remove('hidden');
     
-    let html = `<ul id="content__selection">`
+    let html = `<ul id="content__selection">`;
 
     api.getAllPlaylists().then(playlists => {
-            if (playlists) {
-                for (i = 0; i < playlists.length; i++){
-                    html += PlaylistSelect(playlists[i], i);
-                }
-                content.insertAdjacentHTML('beforeend', html);
+        if (playlists) {
+            for (i = 0; i < playlists.length; i++){
+                html += PlaylistSelect(playlists[i], i);
             }
+            content.insertAdjacentHTML('beforeend', html);
+        }
     });
 }
-
-const songs = [
-    {
-        "id": 0,
-        "songName": "Nothing Else Matters",
-        "releaseYear": "1991",
-        "artists": [
-            "Metallica"
-        ],
-        "albumName": "Metallica",
-        "albumArt": "https://www.albumartexchange.com/coverart/_tn/me/metallica_metallicablackalbum_73p.jpg",
-        "songLength": "6:38"
-    },
-    {
-        "id": 1,
-        "songName": "Nothing Else Matters",
-        "releaseYear": "2021",
-        "artists": [
-            "Miley Cyrus",
-            "WATT",
-            "Elton John",
-            "Yo-Yo Ma",
-            "Robert Trujillo",
-            "Chad Smith"
-        ],
-        "albumName": "The Metallica Blacklist",
-        "albumArt": "https://www.albumartexchange.com/coverart/_tn/me/metallica_themetallicablacklis_eant.jpg",
-        "songLength": "6:35"
-    },
-    {
-        "id": 2,
-        "songName": "Stairway to Heaven",
-        "releaseYear": "1971",
-        "artists": [
-          "Led Zeppelin"
-        ],
-        "albumName": "Led Zeppelin IV",
-        "albumArt": "https://www.albumartexchange.com/coverart/_tn/le/ledzeppelin_ledzeppeliniv12_crcx.jpg",
-        "songLength": "8:02"
-    },
-    {
-        "id": 3,
-        "songName": "Stairway to Heaven",
-        "releaseYear": "1971",
-        "artists": [
-          "Led Zeppelin"
-        ],
-        "albumName": "Led Zeppelin IV",
-        "albumArt": "",
-        "songLength": "8:02"
-    }
-]
 
 renderSelection();
